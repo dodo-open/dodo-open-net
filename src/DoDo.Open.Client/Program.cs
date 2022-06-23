5489e186f0428226e4078e2ea18875d7f321b719
Rhyheart <8120183@qq.com>
2022-06-23 10:09:46 +0800

    完善示例项目

--- src/DoDo.Open.Client/Program.cs
+++ src/DoDo.Open.Client/Program.cs
@@ -2,7 +2,7 @@
 using DoDo.Open.Sdk.Models;
 using DoDo.Open.Sdk.Services;
 
-//开放接口服务
+//接口服务
 var openApiService = new OpenApiService(new OpenApiOptions
 {
     BaseApi = "接口地址",
@@ -11,7 +11,7 @@ var openApiService = new OpenApiService(new OpenApiOptions
 });
 //事件处理服务，可自定义，只要继承EventProcessService抽象类即可
 var eventProcessService = new DemoEventProcessService(openApiService);
-//开放事件服务
+//事件服务
 var openEventService = new OpenEventService(openApiService, eventProcessService, new OpenEventOptions
 {
     IsReconnect = true,
